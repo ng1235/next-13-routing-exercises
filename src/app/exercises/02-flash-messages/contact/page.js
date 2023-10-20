7d38d3c7813ecd2e6fdaf90300e5ef2ead526c62
Matthew Ng <matthewtng.2@gmail.com>
2023-10-20 15:35:20 +0900

    feat: complete exercise 2

--- src/app/exercises/02-flash-messages/contact/page.js
+++ src/app/exercises/02-flash-messages/contact/page.js
@@ -1,17 +1,29 @@
-'use client';
-import React from 'react';
+"use client";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { ToastContext } from "../../../../components/ToastProvider";
 
 function ContactPage() {
+  const router = useRouter();
+  const { createToast } = React.useContext(ToastContext);
+  function handleSubmit(event) {
+    event.preventDefault();
+    router.push("/exercises/02-flash-messages");
+    createToast(
+      "Your message has been received! We'll be in touch shortly",
+      "success"
+    );
+  }
   return (
     <main>
-      <form>
+      <form onSubmit={(event) => handleSubmit(event)}>
         <label htmlFor="name">Name:</label>
         <input id="name" required={true} />
 
         <label htmlFor="message">Message:</label>
         <textarea id="message" />
 
-        <button>Submit</button>
+        <button type="submit">Submit</button>
       </form>
     </main>
   );
--- src/app/exercises/02-flash-messages/page.js
+++ src/app/exercises/02-flash-messages/page.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import Link from 'next/link';
+"use client";
+import React, { useEffect } from "react";
+import Link from "next/link";
+import { ToastContext } from "../../../components/ToastProvider";
 
 function FlashMsgExercise() {
   return (
     <main>
       <h1>Welcome to my website!</h1>
       <p>
-        Got feedback? Please{' '}
-        <Link href="/exercises/02-flash-messages/contact">
-          contact us
-        </Link>
-        .
+        Got feedback? Please{" "}
+        <Link href="/exercises/02-flash-messages/contact">contact us</Link>.
       </p>
     </main>
   );
